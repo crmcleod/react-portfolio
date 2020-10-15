8677ed4aee0fd8d86e0d051aacc0b344fe602435
Craig McLeod <mcleod.craig@outlook.com>
2020-10-15 22:18:53 +0100

    Updates

---
+++ precache-manifest.35fff3c4021c139a0cfab290ad49b73d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffaf4268bfe29f49afd422476d0dbd6d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c8b82ccf78abbd57e1b0",
+    "url": "/home/static/css/main.b50fd7d5.chunk.css"
+  },
+  {
+    "revision": "cbd7ebaca3e00a4e6fc9",
+    "url": "/home/static/js/2.66140d2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.66140d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b82ccf78abbd57e1b0",
+    "url": "/home/static/js/main.9a57d8bc.chunk.js"
+  },
+  {
+    "revision": "5286aa6d8d05b8d7c736",
+    "url": "/home/static/js/runtime-main.dfc6c75d.js"
+  },
+  {
+    "revision": "086f72c4314d499c7316fd728c6a6d65",
+    "url": "/home/static/media/Screenshot 2020-07-29 at 20.48.15.086f72c4.png"
+  },
+  {
+    "revision": "42e05bf52234a01c14965297cdd86d3e",
+    "url": "/home/static/media/Screenshot 2020-07-29 at 21.10.03.42e05bf5.png"
+  },
+  {
+    "revision": "3ecc2a00ac7da419c18e3e9ad764297d",
+    "url": "/home/static/media/Screenshot 2020-07-29 at 21.27.44.3ecc2a00.png"
+  },
+  {
+    "revision": "4d2c09489deb51a99c6be7a98788161a",
+    "url": "/home/static/media/Screenshot 2020-08-04 at 09.33.24.4d2c0948.png"
+  },
+  {
+    "revision": "9ffe5931964e8439af1f95308bd548f0",
+    "url": "/home/static/media/Sherry.9ffe5931.png"
+  },
+  {
+    "revision": "1de7d1d40d19f8f193d4efc13bfb112d",
+    "url": "/home/static/media/capilano.1de7d1d4.JPG"
+  },
+  {
+    "revision": "b9234d91604dff68c5da4590ce19b62f",
+    "url": "/home/static/media/friendotitle3.b9234d91.png"
+  },
+  {
+    "revision": "f67aeaf913a3bca855f1e37fc5087468",
+    "url": "/home/static/media/linkedin1.f67aeaf9.svg"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/home/static/media/logo.ddc25012.svg"
+  },
+  {
+    "revision": "214fdec4d7999b4dd958507d2aad76e2",
+    "url": "/home/static/media/machine-head-1.214fdec4.jpg"
+  },
+  {
+    "revision": "d1de4c5f6cef5058527d4b821c4791f6",
+    "url": "/home/static/media/profile.d1de4c5f.JPG"
+  }
+]);
