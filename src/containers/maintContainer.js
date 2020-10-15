4f7762fa9f7d93239238c021362e9a2ebfb463c7
Craig McLeod <mcleod.craig@outlook.com>
2020-10-15 21:26:36 +0100

    Prevent mobile running

--- src/containers/maintContainer.js
+++ src/containers/maintContainer.js
@@ -6,16 +6,16 @@ const MainContainer = () => {
        const [profileActive, setProfileActove] = useState(true)
        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         return(
-            <h1> Under Construction For Mobile Device</h1>
+            <h1 id="mobile-warning"> Under Construction For Mobile Devices</h1>
         )
-       } 
+       } else {
     return(
         <>
         <div id="card-wrapper">
             <CardContainer  profileActive={ profileActive } />
         </div>
         </>
-    )
-
+        )
+    }
 }
 export default MainContainer
