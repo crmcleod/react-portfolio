4dcc182d66f264395f61cfbc8a0bc6ea4a2a72e8
Craig McLeod <mcleod.craig@outlook.com>
2020-10-15 21:22:01 +0100

    Added function to prevent mobile use ... for now

--- src/containers/maintContainer.js
+++ src/containers/maintContainer.js
@@ -3,7 +3,12 @@ import CardContainer from './cardContainer'
 
 const MainContainer = () => {
 
-    const [profileActive, setProfileActove] = useState(true)
+       const [profileActive, setProfileActove] = useState(true)
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        return(
+            <h1> Under Construction For Mobile Device</h1>
+        )
+       } 
     return(
         <>
         <div id="card-wrapper">
