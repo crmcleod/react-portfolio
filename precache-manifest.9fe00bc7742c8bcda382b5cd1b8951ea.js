a3b8c34ea878e43a0ec233f7b226a3d1715a259a
Craig McLeod <mcleod.craig@outlook.com>
2020-10-22 15:39:59 +0100

    Updates

---
+++ precache-manifest.9fe00bc7742c8bcda382b5cd1b8951ea.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f807436fcb3be8a7bbc118ffc74fc4",
+    "url": "/crmcleod/index.html"
+  },
+  {
+    "revision": "67e36cd40f1d68bf27f5",
+    "url": "/crmcleod/static/css/main.fcd01e1b.chunk.css"
+  },
+  {
+    "revision": "0dd350e7d47cc7a3129c",
+    "url": "/crmcleod/static/js/2.e1a663c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crmcleod/static/js/2.e1a663c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e36cd40f1d68bf27f5",
+    "url": "/crmcleod/static/js/main.e2692427.chunk.js"
+  },
+  {
+    "revision": "5fa03fb8d7c6c5825c2b",
+    "url": "/crmcleod/static/js/runtime-main.b21f2c0a.js"
+  },
+  {
+    "revision": "008399c70de14e827ef27fa6286a88df",
+    "url": "/crmcleod/static/media/Screenshot 2020-07-29 at 20.48.15.008399c7.jpg"
+  },
+  {
+    "revision": "42e05bf52234a01c14965297cdd86d3e",
+    "url": "/crmcleod/static/media/Screenshot 2020-07-29 at 21.10.03.42e05bf5.png"
+  },
+  {
+    "revision": "3ecc2a00ac7da419c18e3e9ad764297d",
+    "url": "/crmcleod/static/media/Screenshot 2020-07-29 at 21.27.44.3ecc2a00.png"
+  },
+  {
+    "revision": "4d2c09489deb51a99c6be7a98788161a",
+    "url": "/crmcleod/static/media/Screenshot 2020-08-04 at 09.33.24.4d2c0948.png"
+  },
+  {
+    "revision": "9ffe5931964e8439af1f95308bd548f0",
+    "url": "/crmcleod/static/media/Sherry.9ffe5931.png"
+  },
+  {
+    "revision": "1de7d1d40d19f8f193d4efc13bfb112d",
+    "url": "/crmcleod/static/media/capilano.1de7d1d4.JPG"
+  },
+  {
+    "revision": "b9234d91604dff68c5da4590ce19b62f",
+    "url": "/crmcleod/static/media/friendotitle3.b9234d91.png"
+  },
+  {
+    "revision": "f67aeaf913a3bca855f1e37fc5087468",
+    "url": "/crmcleod/static/media/linkedin1.f67aeaf9.svg"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/crmcleod/static/media/logo.ddc25012.svg"
+  },
+  {
+    "revision": "214fdec4d7999b4dd958507d2aad76e2",
+    "url": "/crmcleod/static/media/machine-head-1.214fdec4.jpg"
+  },
+  {
+    "revision": "9ac7f72dc52ca75f0dde128b0777c8f3",
+    "url": "/crmcleod/static/media/profile1.9ac7f72d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.52f7c63c623bd6344c265e6ee8f17194.js"
+  "/crmcleod/precache-manifest.9fe00bc7742c8bcda382b5cd1b8951ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crmcleod/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
